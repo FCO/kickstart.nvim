d81669d772666fcf71f88365eb4a18d087a09100
Fernando Corrêa de Oliveira <fernando.correa@humanstate.com>
2024-08-04 01:58:08 +0100

    Add Neogit

--- lua/custom/plugins/git.lua
+++ lua/custom/plugins/git.lua
@@ -1,12 +1,26 @@
-return { -- Adds git related signs to the gutter, as well as utilities for managing changes
-  'lewis6991/gitsigns.nvim',
-  opts = {
-    -- signs = {
-    --   add = { text = '+' },
-    --   change = { text = '~' },
-    --   delete = { text = '_' },
-    --   topdelete = { text = '‾' },
-    --   changedelete = { text = '~' },
-    -- },
+return {
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      -- signs = {
+      --   add = { text = '+' },
+      --   change = { text = '~' },
+      --   delete = { text = '_' },
+      --   topdelete = { text = '‾' },
+      --   changedelete = { text = '~' },
+      -- },
+    },
+  },
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed, not both.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+    },
+    config = true,
   },
 }
